cd9a46fbdc63d98fa629625a68934f4f02e73c58
Jonathan Lynch <jonathanlynch@bitgo.com>
2021-12-02 11:46:32 -0500

    day 2 fixed

--- 2/count_window_increases.py
+++ 2/count_window_increases.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 increases = 0
-previous = 0
 window = []
 with open('input', 'r') as f:
     for line in f.readlines():
         line = int(line.strip())
-        if len(window) == 3:
-            current = sum(window)
-            if previous:
-                if previous < current: increases += 1
-            previous = current
         window.append(line)
-        if len(window) > 3:
+        if len(window) == 4:
+            current = sum(window)
+            if sum(window[:-1]) < sum(window[1:]): increases += 1
             window.pop(0)
 
 print(increases)
